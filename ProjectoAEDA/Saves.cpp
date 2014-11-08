64762a17b36108dae7c36ade359bba6bb4b7e341
José Marques <zezu.marques@gmail.com>
2014-11-08 22:45:16 +0000

    clientes save

--- ProjectoAEDA/Saves.cpp
+++ ProjectoAEDA/Saves.cpp
@@ -10,3 +10,24 @@
 
 using namespace std;
 
+void guardaClientesFicheiro(vector<Cliente *> clientes, string ficheiro)
+{
+	ofstream file;
+
+	file.open(ficheiro.c_str(), ofstream::out);
+
+	if (file.is_open())
+	{
+		vector<Cliente *>::iterator it;
+		file << static_cast<int>(clientes.size()) << endl;
+		for(it = clientes.begin(); it!= clientes.end(); it++)
+		{
+			file << (*it) ->getIDcliente() << endl;
+			file << (*it) ->getNomeCliente() << endl;
+			file << (*it) ->getHorasMes() << endl;
+			file << (*it) ->getMensalidade() << endl;
+
+		}
+
+	}
+}
