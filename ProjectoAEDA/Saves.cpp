2ff0dc3849f3624ab420653de5320bfa27e6e50c
José Marques <zezu.marques@gmail.com>
2014-11-08 17:26:55 +0000

    Includes save to txt

---
+++ ProjectoAEDA/Saves.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+#include "BikeSharing.h"
+#include "Estacao.h"
+#include "Aluguer.h"
+#include "Bicicleta.h"
+#include "Cliente.h"
+#include "Fornecedor.h"
+
+using namespace std;
+
